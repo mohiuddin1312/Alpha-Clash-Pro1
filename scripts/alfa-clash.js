af834d25aba60afe0687d4f04ed2ee960277f107
mohiuddin1312 <mohiuddin1312@gmail.com>
2025-01-05 19:03:27 +0600

    add clash list and remove

--- scripts/alfa-clash.js
+++ scripts/alfa-clash.js
@@ -15,6 +15,9 @@ function continueGame(){
     // set randomly generated alphabet to the screen (show it)
     const currentAlphabetElement = document.getElementById('current-alphabet')
     currentAlphabetElement.innerText = alphabet;
+
+    // set background color 
+    setBackgroundColorById(alphabet);
 } 
 
 function play(){
