6ecce3061e7b536c83a7603e2cbf8ed0eda46798
mohiuddin1312 <mohiuddin1312@gmail.com>
2025-01-05 22:25:39 +0600

    match key with screen

--- scripts/alfa-clash.js
+++ scripts/alfa-clash.js
@@ -8,9 +8,44 @@
 //     playgroundScetion.classList.remove('hidden')
 // }
 
+function handleKeyboardButtonPress(event){
+    const playerpressed = event.key;
+    console.log('player pressed', playerpressed)
+
+    // get the expected to press
+    const currentAlphabetElement = document.getElementById('current-alphabet')
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    console.log(playerpressed, expectedAlphabet);
+
+    // check matched or not
+    if(playerpressed === expectedAlphabet){
+        console.log('you get a point');
+        // update score:
+        //  1. get the current score
+          const currentScoreElement = document.getElementById('current-score');
+          const currentScoreText = currentScoreElement.innerText;
+          const currentScore = parseInt(currentScoreText)
+          console.log(currentScore);
+        // 2. increase the score by 1
+        const newScore = currentScore + 1;
+        //  3. show the updated score
+        currentScoreElement.innerText = newScore;
+        // start a new round
+        console.log('you have pressed correctly', expectedAlphabet)
+        removeBackgroundColorById(expectedAlphabet);
+        continueGame();
+    }
+    else{
+        console.log('you missed. you lost a life')
+    }
+}
+// capture keyboard key press
+document.addEventListener('keyup', handleKeyboardButtonPress)
+
 function continueGame(){
     const alphabet = getRandomAlphabet()
-    console.log('your random alphabet', alphabet);
+    // console.log('your random alphabet', alphabet);
 
     // set randomly generated alphabet to the screen (show it)
     const currentAlphabetElement = document.getElementById('current-alphabet')
