d2b1f9c64d3c58678d7197edf3adf7bd85cdab12
mohiuddin1312 <mohiuddin1312@gmail.com>
2025-01-06 09:07:08 +0600

    updated score by function

--- scripts/alfa-clash.js
+++ scripts/alfa-clash.js
@@ -21,16 +21,25 @@ function handleKeyboardButtonPress(event){
     // check matched or not
     if(playerpressed === expectedAlphabet){
         console.log('you get a point');
+
+        const currentScore = getTextElementValueById('current-score');
+        const updatedScore = currentScore + 1;
+        setTextElementValueById('current-score', updatedScore);
+
+
+
+        // -----------------------------*****-------------------------------
         // update score:
         //  1. get the current score
-          const currentScoreElement = document.getElementById('current-score');
-          const currentScoreText = currentScoreElement.innerText;
-          const currentScore = parseInt(currentScoreText)
-          console.log(currentScore);
+        //   const currentScoreElement = document.getElementById('current-score');
+        //   const currentScoreText = currentScoreElement.innerText;
+        //   const currentScore = parseInt(currentScoreText)
+        //   console.log(currentScore);
+        // const currentScore = getTextElementValueById('current-score');
         // 2. increase the score by 1
         const newScore = currentScore + 1;
         //  3. show the updated score
-        currentScoreElement.innerText = newScore;
+        // currentScoreElement.innerText = newScore;
         // start a new round
         console.log('you have pressed correctly', expectedAlphabet)
         removeBackgroundColorById(expectedAlphabet);
@@ -38,10 +47,19 @@ function handleKeyboardButtonPress(event){
     }
     else{
         console.log('you missed. you lost a life')
+
+        const currentLife = getTextElementValueById('current-life');
+        const updatedLife = currentLife - 1;
+        setTextElementValueById('current-life', updatedLife);
+
+
+
+
+
         // step-1. get the current life number
-        const currentLifeElement = document.getElementById('current-life');
-        const currentLifeText = currentLifeElement.innerText;
-        const currentLife = parseInt(currentLifeText);
+        // const currentLifeElement = document.getElementById('current-life');
+        // const currentLifeText = currentLifeElement.innerText;
+        // const currentLife = parseInt(currentLifeText);
 
         // step-2. reduce the life count
         const newLife = currentLife - 1;
